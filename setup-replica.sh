72d12530a4d39ad9e21f6a8da1f0f5da5d7d7a6d
Ramdhan Hidayat <ramdhan.h@gmail.com>
2023-06-12 21:17:09 +0700

    Update setup-replica

--- setup-replica.sh
+++ setup-replica.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
-  var cfg = {
-    "_id": "rs0",
-    "members": [
-      {
-        "_id": 0,
-        "host": "localhost:27017"
-      }
-    ]
-  };
-  rs.initiate(cfg, { force: true });
-  rs.reconfig(cfg, { force: true });
-EOF
+if [ "$MONGO_INITDB_ROOT_USERNAME" ] && [ "$MONGO_INITDB_ROOT_PASSWORD" ]; then
+    "${mongo[@]}" -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "$rootAuthDatabase" "$@" <<EOJS
+        rs.initiate({
+            _id: "rs0",
+            version: 1,
+            members: [
+                { _id: 0, host : "localhost:27017" },
+                { _id: 1, host : "localhost:27018" },
+                { _id: 2, host : "localhost:27019" }
+            ]
+        })
+EOJS
+fi
